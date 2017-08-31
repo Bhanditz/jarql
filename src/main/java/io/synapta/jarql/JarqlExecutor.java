273e8f978b801e279e1860ce4141546fc2fd0c2c
EmidioStani <emidiostani@gmail.com>
2017-08-31 13:50:39 +0200

    Added queries and xml as input file

--- src/main/java/io/synapta/jarql/JarqlExecutor.java
+++ src/main/java/io/synapta/jarql/JarqlExecutor.java
@@ -35,13 +35,19 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import static io.synapta.jarql.JarqlParser.parse;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 import org.apache.clerezza.rdf.core.serializedform.Serializer;
 import org.apache.clerezza.rdf.jena.storage.JenaGraphAdaptor;
+import org.json.JSONObject;
+import org.json.XML;
 
 /**
  *
@@ -51,21 +57,40 @@ public class JarqlExecutor {
 
     /**
      * @param args the command line arguments
+     * @throws IOException 
      */
 
-    public static void main(String[] args) throws FileNotFoundException {
-        final String jsonFileName = args[0];
+    @SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+    	final String fileType = args[0];
+        final String jsonFileName = args[1];
         final File jsonFile = new File(jsonFileName);
         if (!jsonFile.exists()) {
             System.err.println("File " + jsonFileName + " does not exist");
             System.exit(-1);
         }
-        final FileInputStream jsonIn = new FileInputStream(jsonFile);
-        final String queryFileName = args[1];
+        FileInputStream jsonIn = new FileInputStream(jsonFile);
+        final String queryFileName = args[2];
         final File queryFile = new File(queryFileName);
         if (!queryFile.exists()) {
             System.err.println("File " + queryFileName + " does not exist");
             System.exit(-1);
+        }
+        String jsonPrettyPrintString="";
+        FileInputStream fin = new FileInputStream(jsonFile);
+        byte[] xmlData = new byte[(int) jsonFile.length()];
+        fin.read(xmlData);
+        fin.close();
+        String TEST_XML_STRING = new String(xmlData, "UTF-8");
+        if(fileType.equals("xml")){
+            JSONObject xmlJSONObj = XML.toJSONObject(TEST_XML_STRING);
+            jsonPrettyPrintString = xmlJSONObj.toString(4);
+            File newTextFile = new File("temp.json");
+            FileWriter temp = new FileWriter(newTextFile);
+            temp.write(jsonPrettyPrintString);
+            temp.close();
+            jsonIn = new FileInputStream(newTextFile);
+
         }
         final FileInputStream queryIn = new FileInputStream(queryFile);
         String queryString = new BufferedReader(new InputStreamReader(queryIn)).lines().collect(Collectors.joining("\n"));
